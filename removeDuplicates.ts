09c06121bdc35901f7c05a711320b6ee9d8189e0
krishnabakshi2455 <krishnabakshi2455@gmail.com>
2024-08-31 21:01:29 +0530

    k

---
+++ removeDuplicates.ts
@@ -0,0 +1,15 @@
+function removeDuplicates(arr:number[]) {
+    let uniquevalue:number[]|boolean[] = [];
+    let result:number[]=[];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!uniquevalue[arr[i]]) {
+            uniquevalue[arr[i]] = true
+            result.push(arr[i])
+        }
+        
+    }
+    return ['this is the result ==',result,'this is the duplicate item',uniquevalue]
+
+}
+console.log(removeDuplicates([1, 2, 2, 3, 4,5,5,6,6,7]))
